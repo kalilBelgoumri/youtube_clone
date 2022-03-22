8fe23fb872a5822208d6e39327bc7665995d771f
kalilBelgoumri <kalil.belgoumri@gmail.com>
2022-03-22 17:55:25 +0000

    add logo youtube

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import Navbar from "./components/Navbar";
 
 
 function App() {
   return (
-    <div className="App">
-    <h1>test</h1>
-    </div>
+    <div >
+      <Navbar />
+ 
+     </div>
   );
 }
 
