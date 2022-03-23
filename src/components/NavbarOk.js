d2beed96308410d23afc7726923b298ed9c8758f
kalilBelgoumri <kalil.belgoumri@gmail.com>
2022-03-23 12:03:46 +0000

    update navbar

--- src/components/NavbarOk.js
+++ src/components/NavbarOk.js
@@ -17,48 +17,52 @@ import { Container } from "@mui/material";
 function NavbarOk() {
   const [search, setSearch] = useState("");
   return (
-   <Container>
-    <div className="flex justify-center items-center overflow-hidden pt-2">
-      <div className="flex justify-center items-center">
-        <div className="mb-5 pl-3">
-          <BurgerMenu />
-        </div>
-        <div className="flex pl-7 overflow-hidden">
-          <img className="max-w-[35vh]" src="./youtube.webp" alt="youtube" />
-          <span className="text-[6px] text-gray-600 -m-2 mt-2 mr-3">FR</span>
-        </div>
-      </div>
-      {/* Search */}
-      <div className="flex mx-auto">
-        <div class="font-sans text-black bg-white flex items-center justify-center">
-          <div class="border overflow-hidden flex">
-            <input type="text" class="px-5 py-[3px]" placeholder="Search..." />
-            <button class="flex items-center justify-center px-4 border-l">
-              <SearchIcon />
-            </button>
+    <Container maxWidth>
+      <div className="flex justify-center items-center overflow-hidden ">
+        <div className="flex justify-center items-center">
+          <div className="mb-5 pl-1 pr-4">
+            <BurgerMenu />
+          </div>
+          <div className="flex pl-7 overflow-hidden">
+            <img className="max-w-[100px]" src="./youtube.webp" alt="youtube" />
+            <span className="text-[12px] text-gray-600 -m-4 mt-2 mr-2">FR</span>
           </div>
         </div>
-        <div className="cursor-pointer flex items-center ml-3">
+        {/* Search */}
+        <div className="flex mx-auto">
+          <div className="text-black bg-white flex items-center justify-center">
+            <div className="border border-gray-300 overflow-hidden flex">
+              <input
+                type="text"
+                className="py-[8px] w-[30vw] md:w-[30vw]"
+                placeholder="Rechercher..."
+              />
+              <button className="flex items-center justify-center px-5 bg-gray-50 border-l">
+                <SearchIcon sx={{ color: "gray" }} />
+              </button>
+            </div>
+          </div>
+          <div className="cursor-pointer flex items-center ml-3">
+            <IconButton>
+              <MicSharpIcon fontSize="medium" sx={{ color: "black" }} />
+            </IconButton>
+          </div>
+        </div>
+        <div className="flex items-center justify-between">
           <IconButton>
-            <MicSharpIcon fontSize="small" />
+            <img className="w-8" src="./youtube.svg" alt="youtube" />
+          </IconButton>
+          <IconButton>
+            <CgMenuGridR fontSize="xl" color="black" />
+          </IconButton>
+          <IconButton>
+            <NotificationsNoneIcon fontSize="large" />
+          </IconButton>
+          <IconButton>
+            <AccountCircle fontSize="large" />
           </IconButton>
         </div>
       </div>
-      <div className="flex items-center justify-between">
-        <IconButton>
-          <img className="w-4" src="./youtube.svg" alt="" />
-        </IconButton>
-        <IconButton>
-          <CgMenuGridR fontSize="small" />
-        </IconButton>
-        <IconButton>
-          <NotificationsNoneIcon fontSize="small" />
-        </IconButton>
-        <IconButton>
-          <AccountCircle />
-        </IconButton>
-      </div>
-    </div>
     </Container>
   );
 }
