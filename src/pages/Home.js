c74f3c6f54f6df3b77716468d9ad7e512dbfea99
kalilBelgoumri <kalil.belgoumri@gmail.com>
2022-03-24 22:26:24 +0000

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,13 +8,15 @@ function Home() {
   console.log(posts);
   return (
     <>
-      <div className=" overflow-hidden grid grid-cols-4 gap-4 gap-5">
+      <div className="px-10 py-5 overflow-hidden grid grid-cols-2 xl:grid-cols-4 gap-4 ">
         {posts?.map((post, index) => (
           <>
             {/* <CardMediaYoutube key={index} video={post.id.videoId} /> */}
             <YouTube
-              className="w-96 h-52"
+            containerClassName="hover:-translate-y-1 duration-300 hover:scale-110"
+              className=" w-[27vw] h-28 xl:w-96 xl:h-52"
               videoId={post.id.videoId} // defaults -> null
+              onPlay
               // defaults -> null
             />
           </>
