86a08947770540879d93d1612eaf9c763a57b07e
kalilBelgoumri <kalil.belgoumri@gmail.com>
2022-04-04 12:05:52 +0000

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import React from "react";
 import CardMediaYoutube from "../components/CardMedia";
 import YoutubeContext from "../context/YoutubeContext";
@@ -10,6 +10,7 @@ import {
   MdOutlineKeyboardArrowLeft,
   MdOutlineKeyboardArrowRight,
 } from "react-icons/md";
+
 function Home() {
   const { posts } = useContext(YoutubeContext);
   const uniqid = require("uniqid");
@@ -24,14 +25,12 @@ function Home() {
             size="20"
           />
           {data.map((item) => (
-            <>
-              <li
-                key={uniqid()}
-                className="bg-gray-100 flex justify-center items-center cursor-pointer text-center text-sm truncate px-12 m-3 py-1 border border-gray-300 rounded-full"
-              >
-                {item}
-              </li>
-            </>
+            <li
+              key={uniqid()}
+              className="bg-gray-100 flex justify-center items-center cursor-pointer text-center text-sm truncate px-12 m-3 py-1 border border-gray-300 rounded-full"
+            >
+              {item}
+            </li>
           ))}
           <MdOutlineKeyboardArrowRight
             className="absolute right-0 cursor-pointer"
@@ -39,26 +38,26 @@ function Home() {
             size="20"
           />
         </div>
+
         <div className=" gap-y-3 py-5 md:mr-[1%] lg:mr-2 xl:mr-[1%] grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  ">
           {posts?.map((post) => (
             <div
               key={uniqid()}
               className="flex flex-col w-full hover:-translate-y-1 duration-300 hover:scale-110"
             >
-              {/* <CardMediaYoutube key={index} videoId={post.id.videoId} /> */}
               {
                 <YouTube
                   className=" w-[90%] h-28 xl:w-[90%] xl:h-52"
                   videoId={post.id.videoId}
                 />
               }
-
-              <Paper className="flex justify-end break-all  text-sm cursor-pointer pl-7  text-left pt-3 w-[90%] h-24 xl:h-36">
+              <Paper className="flex justify-end break-all text-sm cursor-pointer pl-7  text-left pt-3 w-[90%] h-24 xl:h-36">
                 {post.snippet.title}
-                {"cursor-pointor" ? <PositionedMenu /> : "hidden"}
+                <PositionedMenu />
               </Paper>
             </div>
           ))}
+          <div />
         </div>
       </div>
     </>
