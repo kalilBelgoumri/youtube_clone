ef1b6e2e83db75d52e6611fae83230f3afff252b
kalilBelgoumri <kalil.belgoumri@gmail.com>
2022-04-05 10:12:37 +0000

    add useCalback in youtube

--- src/context/YoutubeContext.js
+++ src/context/YoutubeContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, useCallback } from "react";
 import axios from "axios";
 
 const YoutubeContext = createContext();
@@ -7,20 +7,34 @@ export const YoutubeProvider = ({ children }) => {
   const [posts, setPosts] = useState([]);
   const [search, setSearch] = useState("");
 
-  useEffect(() => {
-    const fetchData = async () => {
-      await axios
-        .get(
-          `https://www.googleapis.com/youtube/v3/search?maxResults=10&part=snippet&statistics&brandingSettings&contentDetails&contentOwnerDetailssnippet&thumbnails&snippet&field&id&type=video&title&key=${process.env.REACT_APP_GOOGLE_API}`
-        )
-        .then((res) => {
-          setPosts(res.data.items);
-        })
-        .catch((error) => error);
-    };
-    fetchData();
-  }, [search]);
+  // useEffect(() => {
+  //   const fetchData = useCallback (function async ()  {
+  //     await axios
+  //       .get(
+  //         `https://www.googleapis.com/youtube/v3/search?maxResults=10&part=snippet&statistics&brandingSettings&contentDetails&contentOwnerDetailssnippet&thumbnails&snippet&field&id&type=video&title&key=${process.env.REACT_APP_GOOGLE_API}`
+  //       )
+  //       .then((res) => {
+  //         setPosts(res.data.items);
+  //       })
+  //       .catch((error) => error);
+  //   })
+  //   fetchData()
+  // }, [search])
 
+  const fetchData = useCallback(async () => {
+    await axios
+      .get(
+        `https://www.googleapis.com/youtube/v3/search?maxResults=10&part=snippet&statistics&brandingSettings&contentDetails&contentOwnerDetailssnippet&thumbnails&snippet&field&id&type=video&title&key=${process.env.REACT_APP_GOOGLE_API}`
+      )
+      .then((res) => {
+        setPosts(res.data.items);
+      })
+      .catch((error) => error);
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
   return (
     <YoutubeContext.Provider value={{ posts, search, setSearch }}>
       {children}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,5 @@
-import { useContext, useRef } from "react";
+import { useContext } from "react";
 import React from "react";
-import CardMediaYoutube from "../components/CardMedia";
 import YoutubeContext from "../context/YoutubeContext";
 import YouTube from "react-youtube";
 import { Paper } from "@mui/material";
@@ -17,17 +16,17 @@ function Home() {
 
   return (
     <>
-      <div className="pl-[15vw] bg-[#f9f9f9]">
-        <div className="overflow-x-scroll pl-[50vw] pr-[2vw] scrollbar-hide flex justify-center items-center border-b-[1px] border-t-[1px] ">
+      <div className="bg-[#f9f9f9] pl-[16vw]">
+        <div className="flex items-center justify-center overflow-x-scroll border-b-[1px] border-t-[1px] pl-[50vw] pr-[2vw] scrollbar-hide ">
           <MdOutlineKeyboardArrowLeft
-            className="absolute left- cursor-pointer"
+            className="left- absolute cursor-pointer"
             color="black"
             size="20"
           />
           {data.map((item) => (
             <li
               key={uniqid()}
-              className="bg-gray-100 flex justify-center items-center cursor-pointer text-center text-sm truncate px-12 m-3 py-1 border border-gray-300 rounded-full"
+              className="m-3 flex cursor-pointer items-center justify-center truncate rounded-full border border-gray-300 bg-gray-100 px-12 py-1 text-center text-sm"
             >
               {item}
             </li>
@@ -39,19 +38,19 @@ function Home() {
           />
         </div>
 
-        <div className=" gap-y-3 py-5 md:mr-[1%] lg:mr-2 xl:mr-[1%] grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  ">
+        <div className=" grid grid-cols-2 gap-y-3 py-5 md:mr-[1%] lg:mr-2 lg:grid-cols-3 xl:mr-[1%] xl:grid-cols-4  ">
           {posts?.map((post) => (
             <div
               key={uniqid()}
-              className="flex flex-col w-full hover:-translate-y-1 duration-300 hover:scale-110"
+              className="flex w-full flex-col duration-300 hover:-translate-y-1 hover:scale-110"
             >
               {
                 <YouTube
-                  className=" w-[90%] h-28 xl:w-[90%] xl:h-52"
+                  className=" h-28 w-[90%] xl:h-52 xl:w-[90%]"
                   videoId={post.id.videoId}
                 />
               }
-              <Paper className="flex justify-end break-all text-sm cursor-pointer pl-7  text-left pt-3 w-[90%] h-24 xl:h-36">
+              <Paper className="flex h-24 w-[90%] cursor-pointer justify-end break-all  pl-7 pt-3 text-left text-sm xl:h-36">
                 {post.snippet.title}
                 <PositionedMenu />
               </Paper>
